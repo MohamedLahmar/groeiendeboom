47ac433c8e2892e613bc53a449082dd6dd8d45b1
Mohamed Lahmar <mohamed.lahmar@outlook.com>
2020-06-25 12:13:26 +0200

    Added inspiration project from a google developer

---
+++ js/sky.js
@@ -0,0 +1,33 @@
+(function() {
+  function Sky(canvas, dropper) {
+    this.canvas = canvas;
+    this.dropper = dropper;
+    this.clouds = [];
+    this.addCloud('Zorg\nvoor de\nbomen\ngooi uw afval in de\n' +
+        'afvalbak om de bomen.\n\nwater te geven');
+  }
+
+  Sky.prototype.render = function(ctx) {
+    for (var i = 0; i < this.clouds.length; ++i)
+      this.clouds[i].render(ctx);
+  };
+
+  Sky.prototype.update = function(delta) {
+    var toDelete = [];
+    for (var i = 0; i < this.clouds.length; ++i) {
+      if (this.clouds[i].update(delta))
+        toDelete.push(this.clouds[i]);
+    }
+
+    this.clouds = _.difference(this.clouds, toDelete);
+
+    if (Math.random() < 0)
+      this.addCloud();
+  };
+
+  Sky.prototype.addCloud = function(text) {
+    this.clouds.push(new Cloud(this.canvas, this.dropper, text));
+  };
+
+  window.Sky = Sky;
+})();
